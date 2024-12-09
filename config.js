4df8d38bc3aafde4112fc46a0a0d8fc9ec98010f
michels-1 <michelsderwid@gmail.com>
2024-12-09 08:22:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,4 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || "",
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "",
-REPO_NAME: process.env.config.REPO_NAME || "",
-ALIVE_IMG: process.env.ALIVE_IMG ||"",
-ALIVE_MSG: process.env.ALIVE_MSG ||"",
-ANTI_BOT: process.env.ANTI_LINK || "",
-ANTI_LINK: process.env.ANTI_LINK || "",
-ANTI_BAD: process.env.ANTI_BAD || "",
-ALLWAYS_OFFLINE: process.env.ALLWAYS_OFFLINE || "",
-READ_CMD: process.env.READ_CMD || "", 
 };
