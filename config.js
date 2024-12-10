99e5c2e7127a74e1b99e7a710aabeecfa94a2aaa
michels-1 <michelsderwid@gmail.com>
2024-12-10 08:43:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,17 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "",
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "",
+ AUTO_VOICE: process.env.AUTO_VOICE || "true",
+  ANTI_BAD_WORDS_ENABLED: process.env.ANTI_BAD_WORDS_ENABLED || "true",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+  ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "pakayo,huththo").split(','),
+  ANTI_LINK: process.env.ANTILINK || "true",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+  AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+  ALWAYS_TYPING: process.env.ALWAYS_TYPING || "true",
+  ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
+  ANTI_BOT: process.env.ANTI_BOT || "true",
+  ANTI_DELETE: process.env.ANTI_DELETE || "true",
+  packname: process.env.packname || "PONNA TECH",
 };
